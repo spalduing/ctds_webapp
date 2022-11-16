5baaa9b251d0b9956216a0b8a3d7f869536f471b
mdiazc <miguelangel.diaz.kommit@gmail.com>
2022-11-16 10:42:39 -0500

    Fixed: more railways complains

--- src/components/table_select/TableSelect.jsx
+++ src/components/table_select/TableSelect.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
